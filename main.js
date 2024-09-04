8ad1d91ba798a3dc5c3743e6cfb9d1a2d527dfa2
IlyasSam2 <shambazovilyas@gmail.com>
2024-09-04 20:04:52 +0300

    pr2

---
+++ main.js
@@ -0,0 +1,23 @@
+'use strict'
+
+let name = 'Джон';
+let admin = name;
+
+console.log (admin);
+
+let gname = prompt("Напишите название города");
+let god = prompt("Напишите год образования города");
+let nasel = prompt("Напишите население города");
+
+console.log(`Городу ${gname} исполнилось ${2024-god} лет с момента его образования. Население - ${nasel} человек`)
+
+let r = prompt("Введите радиус круга");
+console.log(3.14*r*r)
+
+let a = prompt("Введите число a");
+a = +a;
+let b = prompt("Введите число b");
+b = +b
+
+
+console.log(`Сумма: ${a+b} Разность: ${a-b} Частное: ${a/b} Произведение: ${a*b}`);
